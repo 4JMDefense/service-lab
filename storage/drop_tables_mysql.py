0c41697726fdc16e4716f0f337a0e592c5cefdc8
Ubuntu <ubuntu@ip-172-31-26-32.us-west-2.compute.internal>
2024-12-04 05:03:35 +0000

    storage

--- storage/drop_tables_mysql.py
+++ storage/drop_tables_mysql.py
@@ -1,5 +1,10 @@
+"""
+This script connects to a MySQL database and drops the 'tasks' and 'completed_tasks' tables if they exist.
+"""
+
 import mysql.connector
 
+# Establish connection to the MySQL database
 db_conn = mysql.connector.connect(
     host="localhost",
     user="suser",
@@ -7,11 +12,15 @@ db_conn = mysql.connector.connect(
     database="storage"
 )
 
+# Create a cursor object to execute SQL queries
 db_cursor = db_conn.cursor()
 
-db_cursor.execute('''
-    DROP TABLE IF EXISTS tasks, completed_tasks
-''')
+# Execute SQL command to drop the tables if they exist
+db_cursor.execute('''DROP TABLE IF EXISTS tasks, completed_tasks''')
 
+# Commit the changes to the database
 db_conn.commit()
+
+# Close the database connection
 db_conn.close()
+
