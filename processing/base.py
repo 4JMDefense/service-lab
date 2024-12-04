84050d45a35733a76eb9d299d7fde128cfbf18e5
Ubuntu <ubuntu@ip-172-31-26-32.us-west-2.compute.internal>
2024-12-04 17:19:20 +0000

    asd

--- processing/base.py
+++ processing/base.py
@@ -1,3 +1,11 @@
+"""
+This module defines the SQLAlchemy base class for the application's database models.
+
+It imports the `declarative_base` function from SQLAlchemy and creates a `Base` class
+that can be used as a foundation for all ORM models in the application.
+"""
+
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
